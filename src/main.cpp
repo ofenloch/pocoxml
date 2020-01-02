a1a4623ecb6ccbd42f43cb7767f968850bc8bad4
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-02 23:45:28 +0100

    add random.xml and use it

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,11 @@
 #include <main.h>
 
 int main(int, char**) {
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!\n";   
     
-    
-    
-    Poco::AutoPtr<Poco::XML::Document> pDoc = CXml::parseFile("data/test.xml");
+    Poco::AutoPtr<Poco::XML::Document> pDoc = CXml::parseFile("data/random.xml");
     
     CXml::printSubNodes(pDoc);
-    
-    
     
     return 0;
 }
