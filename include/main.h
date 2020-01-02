4b334c26d27ac85118b64453e7e857234f057991
Oliver Ofenloch <oliver.ofenloch@gea.com>
2020-01-02 21:35:01 +0100

    initial check in

---
+++ include/main.h
@@ -0,0 +1,12 @@
+#ifndef _main_h_
+#define _main_h_
+
+#include <Poco/DOM/Document.h>
+#include <Poco/DOM/DOMParser.h>
+#include <Poco/DOM/Element.h>
+
+
+#define XML_STRING "<?xml version='1.0' encoding='UTF-8'?><!DOCTYPE people_list SYSTEM 'example.dtd'><people_list><person><name>Peter Schojer</name><birthdate>15/03/1976</birthdate><gender>Male</gender></person></people_list>"
+
+
+#endif // #ifndef _main_h_
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include <main.h>
+
+int main(int, char**) {
+    std::cout << "Hello, World!\n";
+
+    std::cout << "Parsing this xml string:\n\n";
+    std::cout << XML_STRING << std::endl << std::endl;
+
+
+
+
+
+    return 0;
+}
