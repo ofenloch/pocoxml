7f244e8ff4aee73e901933cf1708088223126b4d
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-24 06:11:18 +0100

    read file 'test.xml' and save it to a file, duplicate first <person> and write new document to a second file for comparison

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,16 @@ int main(int, char**) {
     Poco::AutoPtr<Poco::XML::Document> pDoc = CXml::parseFile("data/random.xml");
     
     CXml::printSubNodes(pDoc);
+
+    Poco::AutoPtr<Poco::XML::Document> pDoc2 = CXml::parseFile("data/test.xml");
+    Poco::XML::NodeList* persons = pDoc2->getElementsByTagName("person");
+    CXml::saveNodeToFile((Poco::XML::Node*)pDoc2, "test-original.xml");
+    if (persons->length()>0) {
+        Poco::XML::Node* pPerson = persons->item(0);
+        CXml::duplicateNode(pPerson);
+        CXml::printSubNodes(pDoc2);
+        CXml::saveNodeToFile((Poco::XML::Node*)pDoc2, "test-copy.xml");
+    }
     
     return 0;
 }
