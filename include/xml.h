60e9c2de58dca38fd37a69b3299ce6d8c014371f
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-24 06:09:52 +0100

    add some methods: duplicateNode, saveNodeToFile, getAttribute, setAttribute

--- include/xml.h
+++ include/xml.h
@@ -7,8 +7,11 @@
 #include <Poco/AutoPtr.h>
 #include <Poco/DOM/Document.h>
 #include <Poco/DOM/DOMParser.h>
+#include <Poco/DOM/DOMWriter.h>
 #include <Poco/DOM/Element.h>
+#include <Poco/DOM/NamedNodeMap.h>
 #include <Poco/DOM/NodeFilter.h>
+#include <Poco/DOM/NodeList.h>
 #include <Poco/DOM/NodeIterator.h>
 #include <Poco/SAX/InputSource.h>
 #include <Poco/XML/XML.h>
@@ -21,7 +24,35 @@ public:
     getNodeLevel(Poco::XML::Node* pNode);
     static void
     printSubNodes(Poco::XML::Node* pNode);
-    
+    /**
+     * write the node and all subnodes to a
+     * file with given name
+     * return 0 on success
+     */
+    static int
+    saveNodeToFile(Poco::XML::Node* pNode, std::string fileName);
+    /**
+     * make a deep copy/clone of the given node, change the clone's id
+     * and append the copy to the given node's parent node
+     */
+    static void
+    duplicateNode(Poco::XML::Node* pNode);
+    /**
+     * Returns the attribute's value, if the attribute
+     * exists, or an empty string otherwise.
+     */
+    static std::string
+    getAttribute(Poco::XML::Node* pNode, std::string attrName);
+    /**
+     * Adds a new attribute. If an attribute with that name is already present
+     * in the element, its value is changed to be that of the value parameter.
+     * This value is a simple string; it is not parsed as it is being set. So any
+     * markup (such as syntax to be recognized as an entity reference) is treated
+     * as literal text, and needs to be appropriately escaped by the implementation
+     * when it is written out.
+     */
+    static void
+    setAttribute(Poco::XML::Node* pNode, std::string attrName, std::string attrValue);
 }; // class CXml
 
 #endif // #ifndef _xml_h_
--- src/xml.cpp
+++ src/xml.cpp
@@ -36,3 +36,36 @@ CXml::printSubNodes(Poco::XML::Node* pNode) {
         node = it.nextNode();
     }
 }
+
+/*static*/ int
+CXml::saveNodeToFile(Poco::XML::Node* pNode, std::string fileName) {
+    Poco::XML::DOMWriter writer;
+    writer.setNewLine("\n");
+    writer.setIndent(" ");
+    std::ofstream ofs;
+    ofs.open(fileName);
+    writer.writeNode(ofs, pNode);
+    ofs.close();
+    return 0;
+}
+
+/*static*/ std::string
+CXml::getAttribute(Poco::XML::Node* pNode, std::string attrName) {
+    return ((Poco::XML::Element*)pNode)->getAttribute(attrName);
+}
+
+/*static*/ void
+CXml::setAttribute(Poco::XML::Node* pNode, std::string attrName, std::string attrValue) {
+    ((Poco::XML::Element*)pNode)->setAttribute(attrName, attrValue);   
+}
+
+/*static*/ void
+CXml::duplicateNode(Poco::XML::Node* pNode) {
+    // get the node's parent node
+    Poco::XML::Node* pParentNode = pNode->parentNode();
+    // make a deep clone of the node
+    Poco::XML::Node* pNewNode = pNode->cloneNode(true);
+    // change the duplicated node's id
+    setAttribute(pNewNode, "id", getAttribute(pNode,"id")+"_2");
+    pParentNode->appendChild(pNewNode);
+}
