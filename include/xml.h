63c0ea791f8e4687dfea23543a2ac39c034abd4b
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-30 06:31:29 +0100

    add methods normalizeXMLFile and createTestFile

--- include/xml.h
+++ include/xml.h
@@ -1,25 +1,29 @@
 #ifndef _xml_h_
 #define _xml_h_
 
-#include <iostream>
+#include <iostream> // std::cout
 #include <fstream>
+#include <string>  // std::string
+#include <sstream> // std::stringstream, std::stringbuf
 
-#include <Poco/AutoPtr.h>
+#include <Poco/AutoPtr.h> //typedef to Poco::AutoPtr
 #include <Poco/DOM/Document.h>
 #include <Poco/DOM/DOMParser.h>
 #include <Poco/DOM/DOMWriter.h>
-#include <Poco/DOM/Element.h>
 #include <Poco/DOM/NamedNodeMap.h>
 #include <Poco/DOM/NodeFilter.h>
 #include <Poco/DOM/NodeList.h>
 #include <Poco/DOM/NodeIterator.h>
 #include <Poco/SAX/InputSource.h>
+#include <Poco/DOM/Text.h>
 #include <Poco/XML/XML.h>
+#include <Poco/XML/XMLWriter.h>
+#include <Poco/UTF8Encoding.h>
 
 class CXml {
 public:
     static Poco::AutoPtr<Poco::XML::Document>
-    parseFile(std::string fileName); 
+    parseFile(std::string fileName);
     static int
     getNodeLevel(Poco::XML::Node* pNode);
     static void
@@ -53,6 +57,22 @@ public:
      */
     static void
     setAttribute(Poco::XML::Node* pNode, std::string attrName, std::string attrValue);
+
+    /**
+     * Save an XML file in a normalized format to make comparison easier.
+     * 
+     * Parse the given file and write it to a new file. This way,
+     * we should get a "normalized" version of the input file
+     * that can be compared with another xml file in "normalized" format
+     */
+    static int
+    normalizeXMLFile(const std::string& inFileName, const std::string& outFileName);
+
+    /**
+     * create an XML file from scratch
+     */
+    static int
+    createTestFile(const std::string& fileName);
 }; // class CXml
 
 #endif // #ifndef _xml_h_
--- src/xml.cpp
+++ src/xml.cpp
@@ -1,11 +1,13 @@
 #include <xml.h>
 
-/*static*/ Poco::AutoPtr<Poco::XML::Document> 
+/*static*/ Poco::AutoPtr<Poco::XML::Document>
 CXml::parseFile(std::string fileName) {
+
     std::ifstream in(fileName);
     Poco::XML::InputSource src(in);
     Poco::XML::DOMParser parser;
     Poco::AutoPtr<Poco::XML::Document> pDoc = parser.parse(&src);
+
     return pDoc;
 }
 
@@ -13,11 +15,15 @@ CXml::parseFile(std::string fileName) {
  * determine the given node's level in the tree
  * level 0 is the document node
  */
-/*static*/ int CXml::getNodeLevel(Poco::XML::Node* pNode) {
-    if (pNode->parentNode() != nullptr) {
+/*static*/ int 
+CXml::getNodeLevel(Poco::XML::Node* pNode) {
+    if (pNode->parentNode() != nullptr)
+    {
         // there is at list one level above this one
-        return (1+getNodeLevel(pNode->parentNode()));
-    } else {
+        return (1 + getNodeLevel(pNode->parentNode()));
+    }
+    else
+    {
         return 0;
     }
 }
@@ -25,11 +31,11 @@ CXml::parseFile(std::string fileName) {
 /*static*/ void
 CXml::printSubNodes(Poco::XML::Node* pNode) {
     Poco::XML::NodeIterator it(pNode, Poco::XML::NodeFilter::SHOW_ALL);
-    Poco::XML::Node* node = it.nextNode();
+    Poco::XML::Node *node = it.nextNode();
     while (node) {
         int level = CXml::getNodeLevel(node);
         std::cout << level;
-        for (int i=0; i<level; i++ ) {
+        for (int i = 0; i < level; i++) {
             std::cout << " ";
         }
         std::cout << node->nodeName() << "  : " << node->nodeValue() << std::endl;
@@ -42,10 +48,12 @@ CXml::saveNodeToFile(Poco::XML::Node* pNode, std::string fileName) {
     Poco::XML::DOMWriter writer;
     writer.setNewLine("\n");
     writer.setIndent(" ");
+
     std::ofstream ofs;
     ofs.open(fileName);
     writer.writeNode(ofs, pNode);
     ofs.close();
+
     return 0;
 }
 
@@ -56,7 +64,7 @@ CXml::getAttribute(Poco::XML::Node* pNode, std::string attrName) {
 
 /*static*/ void
 CXml::setAttribute(Poco::XML::Node* pNode, std::string attrName, std::string attrValue) {
-    ((Poco::XML::Element*)pNode)->setAttribute(attrName, attrValue);   
+    ((Poco::XML::Element*)pNode)->setAttribute(attrName, attrValue);
 }
 
 /*static*/ void
@@ -66,6 +74,78 @@ CXml::duplicateNode(Poco::XML::Node* pNode) {
     // make a deep clone of the node
     Poco::XML::Node* pNewNode = pNode->cloneNode(true);
     // change the duplicated node's id
-    setAttribute(pNewNode, "id", getAttribute(pNode,"id")+"_2");
+    setAttribute(pNewNode, "id", getAttribute(pNode, "id") + "_2");
     pParentNode->appendChild(pNewNode);
+}
+
+/*static*/ int
+CXml::normalizeXMLFile(const std::string& inFileName, const std::string& outFileName) {
+
+    Poco::AutoPtr<Poco::XML::Document> pDocIn = parseFile(inFileName);
+    Poco::XML::DOMWriter writer;
+    writer.setNewLine("\n");
+    writer.setIndent(" ");
+    int writerOptions = Poco::XML::XMLWriter::WRITE_XML_DECLARATION + Poco::XML::XMLWriter::PRETTY_PRINT + Poco::XML::XMLWriter::PRETTY_PRINT_ATTRIBUTES;
+    writer.setOptions(writerOptions);
+
+    std::ofstream ofs;
+    ofs.open(outFileName);
+    writer.writeNode(ofs, pDocIn);
+    ofs.close();
+
+    return 0;
+}
+
+/*static*/ int
+CXml::createTestFile(const std::string& fileName) {
+
+    // to make things more clear, we call the Document pXMLDoc
+    Poco::AutoPtr<Poco::XML::Document> pXMLDoc = new Poco::XML::Document;
+    // to make things more clear, we call the Document's too pXMLRoot
+    Poco::AutoPtr<Poco::XML::Element> pXMLRoot = pXMLDoc->createElement("DocumentList");
+    pXMLRoot->setAttribute("zzzz","54321");
+    pXMLRoot->setAttribute("wwww","43215");
+    pXMLRoot->setAttribute("aaaa","12345");
+    pXMLDoc->appendChild(pXMLRoot);
+
+    // our first node is a document description <Document> element
+    Poco::AutoPtr<Poco::XML::Element> pDDDoc1 = pXMLDoc->createElement("Document");
+    pDDDoc1->setAttribute("sourcepath", "./data");
+    pDDDoc1->setAttribute("targetpath", "./");
+    pDDDoc1->setAttribute("id", "Documnet1");
+    pXMLRoot->appendChild(pDDDoc1);
+
+    Poco::AutoPtr<Poco::XML::Element> pChild3 = pXMLDoc->createElement("MasterBlock");
+    Poco::AutoPtr<Poco::XML::Text> pText3 = pXMLDoc->createTextNode("text3");
+    pChild3->appendChild(pText3);
+    pDDDoc1->appendChild(pChild3);
+    pChild3->setAttribute("zz", "z");
+    pChild3->setAttribute("aa", "a");
+    pChild3->setAttribute("id", "3");
+
+    Poco::AutoPtr<Poco::XML::Element> pChild1 = pXMLDoc->createElement("MasterBlock");
+    Poco::AutoPtr<Poco::XML::Text> pText1 = pXMLDoc->createTextNode("text1");
+    pChild1->setAttribute("id", "1");
+    pChild1->appendChild(pText1);
+    pDDDoc1->appendChild(pChild1);
+
+    Poco::AutoPtr<Poco::XML::Element> pChild2 = pXMLDoc->createElement("MasterBlockld");
+    Poco::AutoPtr<Poco::XML::Text> pText2 = pXMLDoc->createTextNode("text2");
+    pChild2->setAttribute("id", "2");
+    pChild2->appendChild(pText2);
+    pDDDoc1->appendChild(pChild2);
+
+    Poco::XML::DOMWriter writer;
+    writer.setNewLine("\n");
+    int writerOptions = Poco::XML::XMLWriter::WRITE_XML_DECLARATION +
+                        Poco::XML::XMLWriter::PRETTY_PRINT +
+                        Poco::XML::XMLWriter::PRETTY_PRINT_ATTRIBUTES;
+    writer.setOptions(writerOptions);
+
+    std::ofstream ofs;
+    ofs.open(fileName);
+    writer.writeNode(ofs, pXMLDoc);
+    ofs.close();
+
+    return 0;
 }
