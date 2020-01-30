118a7ae5561e8312c8a1526d8190f17055d926c3
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-30 06:31:59 +0100

    create a test file

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,23 @@
 #include <main.h>
 
-int main(int, char**) {
-    std::cout << "Hello, World!\n";   
-    
+int main(int, char **) {
+    std::cout << "Hello, World!\n";
+
     Poco::AutoPtr<Poco::XML::Document> pDoc = CXml::parseFile("data/random.xml");
-    
+
     CXml::printSubNodes(pDoc);
 
     Poco::AutoPtr<Poco::XML::Document> pDoc2 = CXml::parseFile("data/test.xml");
     Poco::XML::NodeList* persons = pDoc2->getElementsByTagName("person");
     CXml::saveNodeToFile((Poco::XML::Node*)pDoc2, "test-original.xml");
-    if (persons->length()>0) {
+    if (persons->length() > 0) {
         Poco::XML::Node* pPerson = persons->item(0);
         CXml::duplicateNode(pPerson);
         CXml::printSubNodes(pDoc2);
         CXml::saveNodeToFile((Poco::XML::Node*)pDoc2, "test-copy.xml");
     }
-    
+
+    CXml::createTestFile("test-file.xml");
+
     return 0;
 }
-
-
